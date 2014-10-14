d2e2c94e722f77b461769891914e4d9e368ce0ea
Your Name <you@example.com>
2014-10-14 19:33:05 +0100

    Adding bve.

--- valuation.r
+++ valuation.r
@@ -1,24 +1,38 @@
 
 
 calculateROE<- function(){
-  for ( file in   dir()[grep("*output.csv$",dir())]){
+  
+  # WACC needs 
+  files=dir()[grep("Analog.*output.csv$",dir())];
+  nfiles=length(files)
+  
+  for ( file in files){
+    print(file)
     data=read.csv(file);
-    data=data.frame(date=strptime(data$date,"%Y-%m-%d"),shares=data$shares,ebit=data$ebit);
+    data=data.frame(date=strptime(data$date,"%Y-%m-%d"),
+                    shares=data$shares,
+                    bve=data$bve,
+                    ebit=data$ebit);
     require(zoo);
     ebit_next=lag(zoo(data$ebit),1)
     ebit_cur=lag(zoo(data$ebit),0)  
-    roe=ebit_next/ebit_cur-1   
-    dates=(coredata(lag(zoo(data$date),1)))
-    phase=4
+    roe=ebit_next/ebit_cur-1 
+    bve_next=lag(zoo(data$bve),1)
+    bve_cur=lag(zoo(data$bve),0)  
+    bve_average=(bve_next+bve_cur)/2
+    print(bve_average);
+    return(0)
+    dates=(coredata(lag(zoo(data$date),1)));
+    phase=3
+    roes=array();
     for (i in seq(phase,length(roe))){
       coefficients=coef(lm(roe[(i-phase+1):i]~seq(phase)));
       intercept=coefficients[1];
       slope=coefficients[2];
-      print(coefficients);
-      projection=slope*(phase+1)+intercept;
-      y=(c(coredata(roe[(i-phase+1):i]),projection))
-      #plot(seq(phase+1),y)
+      roe_projection=as.double(slope*(phase+1)+intercept);
+      roes[i]=roe_projection;
+      #y=(c(coredata(roe[(i-phase+1):i]),roe_projection));plot(seq(phase+1),y);readline();
     }
-    
+    # evaluate WACC at every stage
   }
 }
