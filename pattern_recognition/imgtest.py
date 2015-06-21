d87ec794ab0bd8df5867770512fc79efefce6811
Your Name <you@example.com>
2015-06-21 15:04:27 +0100

    Adding histogram.

--- pattern_recognition/imgtest.py
+++ pattern_recognition/imgtest.py
@@ -3,6 +3,7 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import pylab
 
 import sys
 
@@ -35,14 +36,13 @@ if nrows * ncols != iar.size:
 # normalize the image
 max_data_reciprocal = 1/float(int(max(iar.data).encode('hex'),16))
 
-print "max_data_reciprocal=",max_data_reciprocal
+#print "max_data_reciprocal=",max_data_reciprocal
 iar = np.multiply(iar,max_data_reciprocal)
 
 # if the median is higher than brightness_median_threshold
 # (i.e. most of the pixels are bright), then invert
 if np.median(iar)> brightness_median_threshold:
     iar = 1-iar; # data is already normalized
-    print " Inverted:",iar
 
 rowdata = iar.tolist()
 
@@ -52,14 +52,26 @@ average_horizbar = np.multiply(([1] * int(nrows * bar_ratio)), 1 / float(nrows))
 if len(average_horizbar) < 2:
     raise SmallImageException()
 
-print "index,sum"
+# convolution example
+# np.convolve(x,average_horizbar)
+
+
+row_res=[]
 for i in range(0, nrows):
     row = rowdata[i];
     if not isinstance(row, list):
         TypeError("row not a list")
-    print i, ",", np.mean(np.convolve(row, average_horizbar))
-    #print row
+    row_res.append(np.mean(row))
 
+print "index,sum"
+for i in range(0,len(row_res)):
+    print i,",",row_res[i]
+
+# The number of bins in the histogram cannot be fixed. We rely on the idea of a
+# typical bangla text.
+
+(n, bins, patches)= pylab.hist(row_res, len(row_res)/5, histtype='step', stacked=True, fill=True)
+pylab.show()
 #plt.imshow(iar)
 
 #print(iar)
