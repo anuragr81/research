1bca9a4e7809aa91b40a6b05c08178254182e32d
Your Name <you@example.com>
2015-09-29 22:43:29 +0100

    Adding snapshot.

--- consumption/worldbank/analyse.r
+++ consumption/worldbank/analyse.r
@@ -1,20 +1,23 @@
 
-
-tseries <- function (filename,field_code)
+# need percentage of people in a certain age-group etc.
+snapshot <- function (filename,field_code,year_code_regex)
 {
-
- dat=read.csv(filename)
- #dat=read.csv('Data_Extract_From_World_Development_Indicators_ALL.csv');
-
-# search a particular field
-xx=dat[dat$Series.Code==field_code,]
-
-#xx=dat[dat$Series.Code=="SP.POP.TOTL",]
-
-x<-as.character(xx$Country.Name)
-y<-as.numeric(as.character(xx$X2000..YR2000.)) # area in 2000
-
-barplot(y,names.arg=x)
-
+  
+  dat=read.csv(filename)
+  #dat=read.csv('Data_Extract_From_World_Development_Indicators_ALL.csv');
+  
+  # search a particular field
+  xx=dat[dat$Series.Code==field_code,]
+  
+  #xx=dat[dat$Series.Code=="SP.POP.TOTL",]
+  result = grep(year_code_regex,names(xx),value=TRUE);
+  if (length(result)==0){
+    stop('Invalid year_code_regex')
+  }
+  
+  x<-as.character(xx$Country.Name)
+  y<-as.numeric(as.character(xx[,result])) # area in 2000
+  barplot(y,names.arg=x,xlab=result)
+  return(y);
 }
 
