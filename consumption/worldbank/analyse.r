fd2c2026632f33525f9eb82902b41482e0f8becb
Your Name <you@example.com>
2015-10-07 17:57:32 +0100

    Adding ofx.csv data.

--- consumption/worldbank/analyse.r
+++ consumption/worldbank/analyse.r
@@ -1,5 +1,8 @@
 
 # need percentage of people in a certain age-group etc.
+# income for every age group would be needed for life-cycle/PI analysis (but aggregate with Friedman's aggregate function can work)
+# Flavin says that if there is a trend in aggregation (per capita income), there would be trend consumption
+
 snapshot <- function (filename,field_code,year_code_regex)
 {
   
