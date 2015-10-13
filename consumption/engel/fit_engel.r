b41c75b736d73fe58223cb59fe5ce29c86741558
Your Name <you@example.com>
2015-10-13 12:37:15 +0100

    Saved all commodity prices.

--- consumption/engel/fit_engel.r
+++ consumption/engel/fit_engel.r
@@ -6,6 +6,8 @@
 # deselecting retired and unoccupied
 # dat[as.numeric(dat$V4)<20,]
 
+require(plyr)
+
 calculateSuperCategory <-function (category){
   return (floor(as.numeric(as.character(category))*1e-4));
 }
@@ -19,12 +21,14 @@ load_expenditure_files<-function (set3_filename, set114_filename){
   
   print(wdiary[1,])
   
-  for (caseno in cases){
-    
+  results=data.frame();
+  count = 1;
+  for (caseno in cases){  
     
     income = as.numeric(as.character(
       winc[as.character(winc$V1)==as.character(caseno),]$V8)
     );
+    
     
     if (!is.na(income)){
       print(paste("Weekly income for caseno",caseno," = ",income));
@@ -36,17 +40,27 @@ load_expenditure_files<-function (set3_filename, set114_filename){
       # the idea would be to i) append the super-category to the frame case_entries 
       
       case_entries$category = calculateSuperCategory(case_entries$V4);
-      print(case_entries);
+      #print(case_entries);
       
       # and then ii) use ddply to aggregate
       dat = ddply(.data=case_entries,.variables=.(V1,V2,V3,category),summarize,
                   expenditure=sum(as.numeric(as.character(V7)))
-                  );
-      print(dat)
-      stop("DONE")
-      print(as.numeric(as.character(case_entries$V7)));
+      );
+      results= rbind(results,dat);
+      #stop("DONE");
+      #print(as.numeric(as.character(case_entries$V7)));
     } #endif na check on income 
-  }
+  }#end for
+  write.csv(results,file='/tmp/results.csv')
+}
+run_regression<-function(x,w_i){
+  # for every caseno i, we have a vector of w_i's (i being a commodity) and outlay x
+  # if we have n casenos and k commodities
+  # then w_i is a [nxk] vector
+  #      x   is a [nx1] vector
+  # The equation w_i = a_i + b_i log(x)
+  # would imply running k regressions such that
+  # w_1[] = a_1 + b_1 log(x)
   
 }
 #
