9a98ca519254cbc52093cefae9156ac4e52da6c8
Your Name <you@example.com>
2015-12-23 19:36:59 +0000

    Adding ecm161 assignment.

--- UofR/ecm101/assignment2/elasticities.r
+++ UofR/ecm101/assignment2/elasticities.r
@@ -4,40 +4,97 @@ require(plyr)#for ddply
 #source('elasticities.r');
 #dat=read_diary('../../../consumption/UKDA-4071-tab/tab/view114.tab')
 
-read_ages<-function(set41_filename)
+read_ages<-function(set41_filename,position)
 {
   ages  = read.table(set41_filename);
   ages = ages [ as.character(ages$V1) !='caseno',];
-  ages = data.frame(caseno=ages$V1,persno=ages$V2,age=as.numeric(ages$V5));
+  if (position==5){
+    ages = data.frame(caseno=ages$V1,persno=ages$V2,age=as.numeric(ages$V5));
+  } else 
+  {
+    if (position == 6){
+      ages = data.frame(caseno=ages$V1,persno=ages$V2,age=as.numeric(ages$V6));
+    } else {
+      stop('Invalid position for set41 file.')
+    }
+  }
   return(ages);
   
 }
 
-analyze <- function(diary_csv_filename,set41_filename){
-  ages = read_ages(set41_filename);
+# example
+# take all 32 year olds who spend 100-120 altogether
+# and see how much they consume
+# in Fat categories{
+# 30401=Butter,
+# 30402=Margarine,
+# 30403=Cooking oil and fats,
+# 30504=Hame and bacon, 
+# 30502=Lamb,
+# 30501=Beef,
+# 30505=Sausages,
+# 30508=Canned Meat,
+# 30509=Cold Meat,
+# 30510=Pies,
+# 305011=Ready Meat, 
+# 30512=Meat type not specified}
+
+# c("30401","30402","30403","30504","30502","30501","30505","30508","30509","30510","305011","30512")
+write_age_expenditure <- function(diary_csv_filename,
+                                  set41_filename,
+                                  age_expenditure_csv_filename,
+                                  set41_age_position){
+  ages = read_ages(set41_filename=set41_filename,position=set41_age_position);
   diary = read.csv(diary_csv_filename);
-  return (merge(diary,ages));
+  m= (merge(diary,ages));
+  write.csv(x=m,file=age_expenditure_csv_filename)
+  return(m);
+}
+
+analyze<-function(age_expenditure_csv_filename){
   
+  dat = read.csv(age_expenditure_csv_filename);
+  
+  total_expenditure= data.frame(caseno=dat$caseno,
+                                persno=dat$persno,
+                                expwk114=dat$expwk114,
+                                total_expenditure=dat$expenditure,
+                                age=dat$age);
+  
+  dat$is_fat = is.element(as.character(dat$ditemcod),
+                          c("30401","30402","30403","30504","30502","30501","30505","30508","30509","30510","305011","30512")
+  );
+  #return(dat);
+  fat_expenditures = ddply(.data=dat,.variables=.(caseno,persno,expwk114,is_fat),summarize,
+                           expenditure=sum(as.numeric(as.character(diteamt))));
+  
+  m=merge(total_expenditure,fat_expenditures);
+  x=m[m$age<35 & m$age>30 & m$total_expenditure<500 & m$total_expenditure>400 & m$is_fat==TRUE,];
+  plot(x$expenditure,xlab='Fat Expenditure',x$total_expenditure-x$expenditure,ylab='Other Expenditure');
+  return(m);
 }
 
 write_diary <- function(set114_filename,csv_filename){
   
   wdiary = read.table(set114_filename);
   dat = wdiary[as.character(wdiary$V1)!="caseno",];
-  dat = data.frame(caseno=dat$V1,
-                   persno=dat$V2,
-                   expwk114=dat$V3,
-                   ditemcod=dat$V4,
-                   dqualif=dat$V5,
-                   dcodecnt=dat$V6,
-                   diteamt=dat$V7);
-
-  # taking week 1 data
-  dat=dat[as.character(dat$expwk114)=="1",];
+  wdiary = data.frame(caseno=dat$V1,
+                      persno=dat$V2,
+                      expwk114=dat$V3,
+                      ditemcod=dat$V4,
+                      dqualif=dat$V5,
+                      dcodecnt=dat$V6,
+                      diteamt=dat$V7);
   
-  dat2 = ddply(.data=dat,.variables=.(caseno,persno,expwk114),summarize,
-               expenditure=sum(as.numeric(as.character(diteamt))));
-  write.csv(x=dat2,file=csv_filename);
-  return(dat2);
+  # taking week 1 data
+  wdiary_wk1=wdiary[as.character(wdiary$expwk114)=="1",];
+  
+  wdiary_expenditures = ddply(.data=wdiary_wk1,.variables=.(caseno,persno,expwk114),summarize,
+                              expenditure=sum(as.numeric(as.character(diteamt))));
+  wdiary = merge(wdiary_wk1,wdiary_expenditures);
+  
+  write.csv(x=wdiary,file=csv_filename);
+  return(wdiary);
+  
 }
 
