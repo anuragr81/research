8c0ce903b5bc43d33d6d3ecc096bc4fe273c940a
anurag <anandmukh@yahoo.com>
2017-01-09 12:02:14 +0000

    Adding merge functionality.

--- logger/easylogger.py
+++ logger/easylogger.py
@@ -4,6 +4,14 @@ import pandas as pd
 import os
 import sys
 
+import logging;
+
+loglevel=logging.INFO
+FORMAT='%(asctime)-15s %(message)s'
+logging.basicConfig(format=FORMAT);
+logger = logging.getLogger("EL");
+logger.setLevel(loglevel)
+
 class StoreKey:
     def __init__(self,**kwargs):
         self.types= dict( (arg,type(kwargs[arg])) for arg in kwargs)
@@ -71,9 +79,18 @@ class LogWriter:
 
 
 def merge_array(arr):
-    if isinstance(arr,list) or isinstance(arr,tuple):
-        return tuple(reduce(merge_data, arr))
+    logger.debug("Entering merged_array - type(arr)="+str(type(arr)))
+    if isinstance(arr,list) or isinstance(arr,tuple) or isinstance(arr,pd.core.series.Series):
+        rd=reduce(merge_data, list(arr))
+        logger.debug("merged_array : given="+str(arr)+" reduced="+str(rd))
+        tpl=tuple(rd.items()) if isinstance(rd,dict) else tuple(rd)
+        logger.debug("merged_array : tuple(rd)="+str(tpl))
+        return tpl
+    elif isinstance(arr,dict):
+        logger.debug("merged_array - returning - tuple - "+str(tuple(arr.items())))
+        return tuple(arr.items())
     else:
+        logger.debug("merged_array - returning - tuple(arr)="+str(tuple(arr)))
         return tuple(arr)
 
 def test_merge_array():
@@ -110,8 +127,9 @@ class LogReader:
             for item in key.items():
                 dfDict[str(item[0])].append(self.formatter.convert_back(storeKey.types[item[0]],item[1]))
         for value in allValues:
-            dfDict['value'].append(json.dumps(value))
-
+            logger.debug("adding value="+str(value)+" of type="+str(type(value)))
+            #dfDict['value'].append(json.dumps(value))
+            dfDict['value'].append(value)
         rawDf=pd.DataFrame(dfDict)
 
         #TODO: aggregate the rawDf based on storeKey - using aggregate method or by creating a new df by
@@ -126,33 +144,47 @@ class LogReader:
 
 
 def merge_data(a,b):
+    logger.debug( "entering merge_data - a="+str(a)+" b="+str(b))
     if isinstance(a,list):
         if isinstance(b,list) :
-            return a+b
+            mergedArray = merge_array(a + b)
+            logger.debug("a="+str(a)+" b="+str(b)+" returning - a+b="+str(mergedArray))
+            return mergedArray
         else:
-            return a+ [b]
+            mergedArray=a+ [b]
+            logger.debug("a="+str(a)+" b="+str(b)+" returning - a+ [b]= "+str(mergedArray))
+            return mergedArray
     elif isinstance(a,dict):
         if isinstance(b,list):
-            return b + [a]
+            mergedArray=[a]+b
+            logger.debug("a="+str(a)+" b="+str(b)+" returning - [a]+b="+str(mergedArray))
+            return mergedArray
         elif isinstance(b,dict):
             aKeys = set(a.keys())
             bKeys= set(b.keys())
             resDict = dict((k,merge_data(a[k],b[k])) for k in aKeys.intersection(bKeys))
-            #print "Created - ", resDict
-            #print "Updating with - ", dict((k,a[k]) for k in aKeys.difference(bKeys))
+            logger.debug("Created - "+str(resDict))
+            logger.debug("Updating with - "+str(dict((k,a[k]) for k in aKeys.difference(bKeys))))
             resDict.update(dict((k,a[k]) for k in aKeys.difference(bKeys)))
-            #print "Updating with - ", dict((k,b[k]) for k in bKeys.difference(aKeys))
+            logger.debug("Updating with - "+str(dict((k,b[k]) for k in bKeys.difference(aKeys))))
             resDict.update(dict((k,b[k]) for k in bKeys.difference(aKeys)))
+            logger.debug("a="+str(a)+" b="+str(b)+" returning resDict="+str(resDict))
             return resDict
         else:
             raise ValueError("Cannot merge atomic value with a dictionary")
     else:
         if isinstance(b,list):
-            return [a]+b
+            mergedArray = [a]+b
+            logger.debug("a="+str(a)+" b="+str(b)+" returning - [a]+b="+str(mergedArray))
+            return mergedArray
+
         elif isinstance(b,dict):
             raise ValueError("Cannot merge atomic value with a dictionary")
         else:
-            return [a,b]
+            mergedArray = [a,b]
+            logger.debug("a="+str(a)+" b="+str(b)+" type(a)="+str(type(a))+" type(b)="+str(type(b))+" returning - [a,b]="+str(mergedArray))
+            return mergedArray
+
 
 def test_merge_data():
     print (merge_data([1], 2) == [1, 2])
@@ -173,9 +205,12 @@ def test_merge_data():
     print (merge_data({1: 3, 3: 5}, {1: [3, 4], 2: 3}))
     print (merge_data({1: 3, 3: 5}, [1]))
     print (merge_data([1],{1: 3, 3: 5}))
+    print (merge_data([{1:3}],[{1:4}]))
 
 
 if __name__=="__main__":
+    #test_merge_data()
+    #print merge_array([[{"y": [55, 63]}], {"y": [67]}])
     #sys.exit(0)
     k=StoreKey(date=datetime.date(2015,1,5),tag="as")
 
@@ -187,12 +222,13 @@ if __name__=="__main__":
 
     if toWrite:
         l = LogWriter(storeKey=k,fname=fname)
-        l.log(StoreKey(tag="CAMP",date=datetime.date(2015,3,3)),2,3,x=3,y=[55,63])
-        l.log(StoreKey(tag="CAMP",date=datetime.date(2015,3,3)),2,4,x=4,y=[67])
-        l.log(StoreKey(tag="CAMP",date=datetime.date(2015,3,4)),2,4,x=4)
+        l.log(StoreKey(tag="CAMP",date=datetime.date(2015,3,3)),y=[55,63],x={2:3})
+        l.log(StoreKey(tag="CAMP",date=datetime.date(2015,3,3)),y=[67],x={1:2})
+        l.log(StoreKey(tag="CAMP",date=datetime.date(2015,3,4)),y=4,)
     else:
         l = LogReader(fname)
         df = l.load(StoreKey(tag="CAMP",date=datetime.date(2015,3,4)))
         print df
+
 
 
