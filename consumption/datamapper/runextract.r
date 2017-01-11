0955dba265c904262874579d953092bb9c7d803d
anurag <anandmukh@yahoo.com>
2017-01-11 08:51:23 +0000

    Adding cex handlers.

--- consumption/datamapper/runextract.r
+++ consumption/datamapper/runextract.r
@@ -2,7 +2,10 @@
 setwd('c:/local_files/research/consumption/datamapper/')
 
 source('callertree.r')
-
 source('./sa/sa.r')
+
+source('us_cex/us_cex_loader.r')
+#C:/local_files/research/consumption/cex/cex_data/
+#source('us_cex/us_cex_loader.r');ds<-uscex(fu=fu)@combined_data_set(2004,"C:/local_files/research/consumption/cex/cex_data/",201,FALSE)
 
 print("DONE")
