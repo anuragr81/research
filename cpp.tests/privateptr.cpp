009679fe215b117b4b8592fc5e2aa12fdd4aec9d
anuragr81 <anandmukh@yahoo.com>
2017-10-20 11:53:20 +0100

    Adding private ptr.

--- cpp.tests/privateptr.cpp
+++ cpp.tests/privateptr.cpp
@@ -66,6 +66,15 @@ int main(){
     for (int  i = 0 ; i < b.size() ; ++i){
             cout <<  b.get_A(i).get_a() << endl;
     } 
+    try { 
+    A1 & a1 = dynamic_cast<A1&> (const_cast<A&>(b.get_A(0)));
+    const A1 & a1c = dynamic_cast<const A1&> ((b.get_A(0)));
+    A2 & a2 = dynamic_cast<A2&> (const_cast<A&>(b.get_A(1)));
+    const A2 & a2c = dynamic_cast<const A2&> ((b.get_A(1)));
+    
+    } catch (std::exception & e){
+         cout << " failure " << endl;
+    } 
     // b.get_A(0).set_a(-1);// won't compile
     return 0;
 }
