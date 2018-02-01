0e2f357f990d3d8837eb8ef284586a6264cfb31d
anurag <anandmukh@yahoo.com>
2018-02-01 22:58:47 +0000

    Adding calories dictionary.

--- consumption/lsms/lsms_categories.py
+++ consumption/lsms/lsms_categories.py
@@ -2,15 +2,6 @@ import pydot
 
 expenseTree= {
 'intoxication' :['cigarettes','beer','brews','winespirits'],
-'food': {'beverages':['water','tea','coffee','miscdrinkpowder','canned_drink','readymade_tea','readymade_coffee',],
-         'starch': ['milling','rice_husked','rice_paddy','maize_green','maize_grain','maize_flour','millet_grain','millet_flour','wheat','bread','bunscakes','pasta','othercereal','pulses',] , 
-         'vegstarch' : ['cassava_fresh','cassava_flour','sweet_potato','yam','potatoes','banana_green','othervegstarch'], 
-         'sugars':['sugar','sweet','honey'], 
-         'fat':['peanuts','coconut','cashew_almonds','nut_products','cooking_oil','butter_margarine',],
-         'vegetables':['onion','greens','dried_canned_veg',] ,
-         'fruits':['banana_ripe','citrus','mangoes','sugarcane'],
-         'protein':['goat','beef','pork','chicken','wild_birds','wild_meat','fish_seafood','dried_canned_fish','packaged_fish','milk','milk_products','canned_mik'], 
-         'condiments': ['spices','salt'] },
 'home_energy':['charcoal','electricity','gas','kerosene','matches'],
 'transport':['petrol','public_transport',],
 'communications':['cellphone_voucher','phone',],
@@ -23,8 +14,65 @@ expenseTree= {
 'housing':['mortgage','council_rates','building_material','bamboo','grass'],
 'legalfinance':['insurance','legal'],
 'social_events':['marriage','bride_price','funeral'],
+'food': {'beverages':['water','tea','coffee','miscdrinkpowder','canned_drink','readymade_tea','readymade_coffee',],
+         'starch': ['rice_husked','rice_paddy','maize_green','maize_grain','maize_flour','millet_grain','millet_flour','wheat','bread','bunscakes','pasta','othercereal','pulses',] ,  # milling
+         'vegstarch' : ['cassava_fresh','cassava_flour','sweet_potato','yam','potatoes','banana_green','othervegstarch'], 
+         'sugars':['sugar','sweet','honey'], 
+         'fat':['peanuts','coconut','cashew_almonds','nut_products','cooking_oil','butter_margarine',],
+         'vegetables':['onion','greens','dried_canned_veg',] ,
+         'fruits':['banana_ripe','citrus','mangoes','sugarcane'],
+         'protein':['goat','beef','pork','chicken','wild_birds','wild_meat','fish_seafood','dried_canned_fish','packaged_fish','milk','milk_products','canned_mik'], 
+         'condiments': ['spices','salt'] },
 }
 
+caloriesDict =  { 'rice_paddy': { 'calories': 360.*.8 },
+		  'rice_husked': { 'calories': 360. },
+		  'maize_green': { 'calories': 106. },
+		  'maize_flour': { 'calories': 364. },
+		  'maize_grain': { 'calories': 364. },
+		  'millet_grain': { 'calories': 378. },
+		  'millet_flour': { 'calories': 382. },
+		  'wheat'       : {'calories': 327. },
+		  'bread'       : {'calories': 267. },
+		  'bunscakes'   : {'calories':324. },
+		  'pasta'       : {'calories': 352. },
+		  'othercereal' : {'calories': 300. },
+		  'pulses'      : {'calories': 352. },
+		  'cassava_fresh'       : {'calories': 160. },
+		  'cassava_flour'       : {'calories': 159. },
+		  'sweet_potato'        : {'calories': 86. },
+		  'yam'                 : {'calories': 118. },
+		  'potatoes'            : {'calories': 77. },
+		  'banana_green'        : {'calories': 100. },
+		  'othervegstarch'      : {'calories': 100. },
+		  'sugar'               : {'calories': 387. },
+		  'sweet'               : {'calories': 350. },
+		  'honey'               : {'calories': 300. },
+		  'peanuts'             : {'calories': 567. },
+		  'coconut'             : {'calories': 350. },
+		  'cashew_almonds'      : {'calories': 550. },
+		  'nut_products'        : {'calories': 550. },
+		  'cooking_oil'         : {'calories': 880. },
+		  'butter_margarine'    : {'calories': 720. },
+		  'onion'               : {'calories': 40. },
+		  'greens'              : {'calories': 30. },
+		  'dried_canned_veg'    : {'calories': 40. },
+		  'banana_ripe'         : {'calories': 100. },
+		  'citrus'              : {'calories': 45. },
+		  'mangoes'             : {'calories': 60. },
+		  'sugarcane'           : {'calories': 180. },
+		  'goat'                : {'calories': 109. },
+		  'beef'                : {'calories': 250 . },
+		  'pork'                : {'calories': 300. },
+		  'chicken'             : {'calories': 160. },
+		  'wild_birds'          : {'calories': 160. },
+		  'fish_seafood'        : {'calories': 100. },
+		  'dried_canned_fish'   : {'calories': 150. },
+		  'packaged_fish'       : {'calories': 100. },
+		  'milk'                : {'calories': 40. },
+		  'milk_products'       : {'calories': 100. },
+		  'canned_milk'         : {'calories': 40. },
+	        }
 
 	#graph.write_png('c:/temp/'+str(i)+".png")
 	#graph.write_png('file'+str(i)+".png")
@@ -43,4 +91,4 @@ graph = pydot.Dot(graph_type='graph')
 res= parse_tree("ET",{'ET':expenseTree})
 
 #print parse_tree("ET",{"one":"One"})
-graph.write_png('c:/temp/test.png')
+#graph.write_png('c:/temp/test.png')
