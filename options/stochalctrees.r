5ed8c85b02eae05ea44df7f564c6f1c6251750ca
argmin <anandmukh@yahoo.com>
2018-04-30 16:12:49 +0100

    Update stochalctrees.r

--- options/stochalctrees.r
+++ options/stochalctrees.r
@@ -1,3 +1,24 @@
+
+
+generate_tree_num <-function (u,d,S0,n){
+  
+  if (abs( u*d-1) >1e-7){
+    stop("Invalid u,d")
+  }
+  
+  prevGrid <- S0
+  for ( i in seq(1,n)){
+    curLevel <- combn(i+1,i)
+    #expand.grid(c("u","d"),c("u","d"),c("u","d"))
+    #mdply to combine 
+    f <-function(...) { dat<-list(...);return(prod(as.numeric(dat)));}
+    prevGrid<- mdply(expand.grid(prevGrid,c(u,d)),f)[,c("V1")]
+    print(prevGrid)
+  }
+  
+  
+} 
+
 generate_tree <-function (u,d,S0,n){
   
   if (abs( u*d-1) >1e-7){
