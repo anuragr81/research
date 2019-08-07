e14b53144b59a46de6eabcc110de76c9f4af4572
anurag <anandmukh@yahoo.com>
2019-08-07 10:29:47 +0100

    Adding regression for price.

--- consumption/datamapper/lsms/lsms_analyse.r
+++ consumption/datamapper/lsms/lsms_analyse.r
@@ -1,11 +1,28 @@
 setwd('c:/local_files/research/consumption/datamapper/')
 source('translation/frameutils.R');source('lsms/lsms_normalizer.r');source('lsms/lsms_loader.r');ll=lsms_loader(fu=fu,ln=lsms_normalizer)
 
-#m2010<-ll@load_market_prices(year = 2010, dirprefix = "../",fu = fu, ln = lsms_normalizer,use_pieces = FALSE)
-#regs<-as.character(unique(m2010$region))
-#items<-as.character(unique(m2010$shortname))
-#iname="beef";par(mfrow=c(5,5));sapply(regs,function(x) { print(x); g=plot_commodity(sname = iname,data=subset(m2010,shortname==iname & region==as.character(x)),year=paste(2010,"- region:",x)) } );
-#for (x in regs) {g=plot_commodity(sname = iname,data=subset(m2010,shortname==iname & region==as.character(x)),year=paste(2010,"- region:",x)) }
+get_regressed_price <- function (lwp,price){
+  if (length(unique(price))>=3 && any(abs(diff(price))>0) && any(abs(diff(lwp))>0)){
+    invsqr         <- 1/lwp**2
+    lmres          <- lm(price~ invsqr)
+    reg_price      <- (lmres$coefficients[[1]]+lmres$coefficients[[2]])
+  } else if (length(price)==0){
+    stop("No prices")
+  } else if (length(unique(price))<3 || !any(abs(diff(price))>0) || !any(abs(diff(lwp))>0) ){
+    return(median(price))
+  } else {
+    stop("Unhandled block")
+  }
+  
+  if (reg_price <0 || abs(log(median(price)/reg_price,2))>1 ) {
+    return(median(price))
+  } else {
+    return(reg_price)
+  }
+
+  
+}
+
 plot_commodity <- function(sname,data,year)
 {
   #print(paste("sname=",sname,"year=",year))
@@ -19,23 +36,30 @@ plot_commodity <- function(sname,data,year)
   
   dat <- subset(data,shortname==sname)
   if (dim(dat)[1]>1 && any(abs(diff(dat$price))>0) ){
-  lmres <- lm(data=dat,price~lwp)
-  plot(dat$lwp,dat$price,xlab = "quantity", ylab="price",main=plotname)
-  if (any(abs(diff(dat$lwp))!=0)) {
-    abline(lmres)
-  } else {
-    print("Could not plot line")
+    dat$invsqr <- with(dat,1/lwp**2)
+    lmres <- lm(data=dat,price~ invsqr)
+    plot(dat$invsqr,dat$price,xlab = "1/(q*q)", ylab="mp",main=plotname)
+    if (any(abs(diff(dat$lwp))!=0)) {
+      abline(lmres)
+    } else {
+      print("Could not plot line")
+    }
+    
+    return(lmres$coefficients[[1]]+lmres$coefficients[[2]])
+    #return(lmres)
   }
   
-  return(lmres$coefficients[[1]]+lmres$coefficients[[2]])
-  #return(lmres)
-  }
-  
-  else if (dim(dat)[1]==0 || !any(abs(diff(dat$price))>0)){
-    print(paste("No prices - sname=",sname,"year=",year))
-    return(NULL)
-  } else {
-    print("Single price entry")
-    return(dat$price[1])
-  }
-}
+}  
+#m2010<-ll@load_market_prices(year = 2010, dirprefix = "../",fu = fu, ln = lsms_normalizer,use_pieces = FALSE)
+#regs<-as.character(unique(m2010$region))
+#items<-as.character(unique(m2010$shortname))
+#iname="beef";par(mfrow=c(5,5));sapply(regs,function(x) { print(x); g=plot_commodity(sname = iname,data=subset(m2010,shortname==iname & region==as.character(x)),year=paste(2010,"- region:",x)) } );
+#for (x in regs) {g=plot_commodity(sname = iname,data=subset(m2010,shortname==iname & region==as.character(x)),year=paste(2010,"- region:",x)) }
+
+#region_district_consumed_items <- unique(merge(unique(o2010[,c("region","district","hhid")]),unique(c2010[,c("hhid","shortname","item")],by=c("hhid")))[,c("region","district","shortname","item")])
+#market_prices_district <- ddply(m2010,.(region,district,shortname,price),summarise,nprices=length(price[!is.na(price)]),median_price=median(price[!is.na(price)]))[,c("region","district","shortname","median_price","nprices")]
+#matched_district_prices <- merge(market_prices_district,region_district_consumed_items,by=c("region","district","shortname"),all.y=TRUE)
+
+#market_prices_regional <- ddply(m2010,.(region,shortname),summarise,reg_price=get_regressed_price(lwp=lwp,price=price))[,c("region","shortname","reg_price")]
+
+#k<-merge(market_prices_regional,matched_district_prices,by=c("region","shortname"),all.y=TRUE)
