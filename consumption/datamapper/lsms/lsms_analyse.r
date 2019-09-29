4d54dbcdf953640345558399d096b6fd27a15951
anurag <anandmukh@yahoo.com>
2019-09-29 08:38:25 +0100

    Adding expensiveregion.

--- consumption/datamapper/lsms/lsms_analyse.r
+++ consumption/datamapper/lsms/lsms_analyse.r
@@ -41,8 +41,10 @@ read_stata_aids_results <- function(r,read_coeff){
 
 
 all_asset_scores <- function(years,dirprefix,fu,ln,ll){
-  assetnames_transport <- c( 'mobile', 'waterheater', 'sewingmachine', 'bed', 'camera', 'watch', 'phone', 'musicplayer', 'videoplayer', 'musicsystem', 'chair', 'table', 'cupboard', 'sofa', 'ac_fan', 'waterpump', 'tv', 'dishtv', 'computer', 'sports_hobby', 'refrigerator', 'land', 'house')
-  assetnames_household <- c('bike', 'motorbike', 'car')
+  assetnames_transport   <- c('bike', 'motorbike', 'car')
+  
+  assetnames_household   <- c(  'sewingmachine', 'bed',  'watch',  'chair', 'table', 'cupboard', 'sofa','sports_hobby','land', 'house')
+  assetnames_electric     <- c('mobile', 'waterheater','camera', 'phone', 'musicplayer', 'videoplayer', 'musicsystem', 'ac_fan', 'waterpump', 'tv', 'dishtv', 'computer',  'refrigerator' )
   out <- NULL
   for (yr in years){
     cdat <- ll@load_diary_file(dirprefix = dirprefix, year = yr, fu = fu, ln = ln )
@@ -51,12 +53,16 @@ all_asset_scores <- function(years,dirprefix,fu,ln,ll){
                                            c("asset_score"="trans_asset_score"))
     asset_scores_household <- plyr::rename(ll@get_asset_score(year = yr, diaryData = cdat, assetsData = adat, assetsList = assetnames_household, ln = ln ),
                                            c("asset_score"="hh_asset_score"))
+    asset_scores_electric    <- plyr::rename(ll@get_asset_score(year = yr, diaryData = cdat, assetsData = adat, assetsList = assetnames_electric, ln = ln ),
+                                      c("asset_score"="elec_asset_score"))
     asset_scores <- merge(asset_scores_household,asset_scores_transport,by=c("hhid"))
+    asset_scores <- merge(asset_scores, asset_scores_electric, by = c("hhid"))
     asset_scores$year <- yr
     out  <- rbind(out,asset_scores)
   }
   out$norm_trans_asset_score <- (out$trans_asset_score - min(out$trans_asset_score)) / ( max(out$trans_asset_score) - min(out$trans_asset_score) )
   out$norm_hh_asset_score <- (out$hh_asset_score - min(out$hh_asset_score)) / ( max(out$hh_asset_score) - min(out$hh_asset_score) )
+  out$norm_elec_asset_score <- (out$elec_asset_score - min(out$elec_asset_score)) / ( max(out$elec_asset_score) - min(out$elec_asset_score) )
   return(out)
 }
 
