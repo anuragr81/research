fd252a32d71155a56f379fc77837e4abf13b2093
Anurag S <anandmukh@yahoo.com>
2020-11-27 20:45:41 +0000

    Adding .Rprofile for local installation of libraries.

---
+++ consumption/datamapper/.Rprofile
@@ -0,0 +1,54 @@
+# set up the CRAN repository to download from
+local({
+  r <- getOption("repos")
+  r["CRAN"] <- "https://cloud.r-project.org/"
+  options(repos = r)
+})
+
+# set up the libPaths to include the library in the R installation
+# and the local library, but to exclude the defauilt one set up by RStudio
+local.lib.path <- file.path(getwd(), "Rlib")
+.libPaths(local.lib.path)
+
+current.lib.paths <- .libPaths()
+
+new.libs <- c()
+for (i in 1:length(current.lib.paths)) {
+  if (!grepl("win-library", current.lib.paths[i])) {
+    new.libs <- c(new.libs, current.lib.paths[[i]])
+  }
+}
+
+.libPaths(new.libs)
+
+final.libs <- c(new.libs, local.lib.path)
+
+.libPaths(final.libs)
+
+print(.libPaths())
+
+library("utils")
+
+# download libraries that are required
+packages <- c("dplyr","moments","plyr","haven")
+
+for (i in 1:length(packages)) {
+  if (packages[i] %in% rownames(installed.packages(local.lib.path)) == FALSE) {
+    print(sprintf("Loading library: %s", packages[i]))
+    install.packages(packages[i], local.lib.path, quiet = T)
+  } else {
+    print(sprintf("Library %s already loaded", packages[i]))
+  }
+}
+
+
+# assign home directory to where this .Rprofile is stored
+home.directory <- getwd()
+options(stringsAsFactors = FALSE)
+
+rm(i)
+rm(final.libs)
+rm(new.libs)
+rm(current.lib.paths)
+# rm(packages)
+rm(local.lib.path)
--- utils/compare_dict.py
+++ utils/compare_dict.py
@@ -14,47 +14,67 @@ def get_end_nodes(root: dict, path =None):
         return reduce(lambda x,y : x + y , [get_end_nodes(path=path+k,root=root[k]) for k in root],[])
     elif isinstance(root,(list,set)):
         path = '' if path is None else path
-        return reduce(lambda x,y : x + y, [get_end_nodes(path=path,root=k) for k in root],[])
+        return reduce(lambda x,y : x + y, [get_end_nodes(path=path+ "[" + str(i) + "]",root=k) for i,k in enumerate(root)],[])
     else:
         return [path]
     
     
+def vector_add(x,y):
+    if isinstance(x,list) or isinstance(y,tuple):
+        if isinstance(y,list) or isinstance(y,tuple):
+            return x + y
+        else:
+            return x + [y]
+    else:
+        if isinstance(y,list) or isinstance(y,tuple):
+            return [x] + y
+        else:
+            return [x] + [y]
+        
 def update_dict (x ,y ):
     a = copy.deepcopy(x)
-    a.update(y)
+    common_keys = set(x.keys()).intersection(set(y.keys()))
+    
+    if common_keys:
+        common_key_dict = dict( (ck,vector_add(x[ck],y[ck])) for ck in common_keys)
+        uncommon_key_dict_x = dict ( (nckx,x[nckx]) for nckx in x if nckx not in common_keys)
+        uncommon_key_dict_y = dict ( (ncky,x[ncky]) for ncky in y if ncky not in common_keys)
+        a = common_key_dict
+        a.update(uncommon_key_dict_x)
+        a.update(uncommon_key_dict_y)
+        return a
+    else:
+        a.update(y)
+    
     return a
     
 def create_dict(et):
-    result ={}
-    print (et.tag + " has %d elements" % len(et))
+    result ={}    
     for i in et:
         if len(i):
             result = update_dict(result, create_dict(i))
-            print(result)
-            print("<<RESULT")
         else:
             items = dict( (k,v) for k,v in i.items())
-#           print ("Element - dict:%s text=%s" % (str(items), i.text) )
+
             if i.text and items:
-                raise ValueError("Both value and attributes are not supported")
-            if i.text:
+                result = update_dict(result,update_dict({i_tag:i.text},{i.tag:items}))
+            elif i.text:
                 result = update_dict(result,{i.tag: i.text})
-            if items:
+            elif items:
                 result = update_dict(result,{i.tag: items})
-                
-            print(result)
-            print("<< NODE RESULT")
+              
             
     return {et.tag: result}
 
-a = '<xml> <a attr1="hello1"> <aa attr2="namaste"></aa> </a><b name="Alpha" /> </xml>'
+a = '<xml> <a attr1="hello1"> <aa attr2="namaste"></aa> </a><b name="Alpha" /><b name="beta" /> </xml>'
 b = '<xml> <a attr1="hello2"><aa>K</aa> </a> </xml>'
 
 #print(compare_dict(a,b))
 
 
 dictionary = create_dict(etree.fromstring(a))
-#end_nodes = get_end_nodes(dictionary)
+end_nodes = get_end_nodes(dictionary)
+print(end_nodes)
 #for e in end_nodes :
 #    node_obj = jsonpath_ng.parse(e)
 #    print(node_obj)
