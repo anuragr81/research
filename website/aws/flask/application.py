7ccae33a35eda89540ecbb054e621b45c30f483a
Anurag S <anurag.srivastava@riskcare.com>
2021-12-03 13:02:59 +0000

    Adding website changes. Reviewing v0.

--- website/aws/flask/application.py
+++ website/aws/flask/application.py
@@ -1,21 +1,43 @@
-from flask import Flask
 
-# print a nice greeting.
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, redirect, url_for, render_template
+
+"""
+
+
+# python anywhere
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+"""
+
+
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='images/' + filename), code=301)
+
+
 def display_index():
-    dat =""
-    with open('index.html') as fh:
-        dat = fh.read()
-    return dat
+    #dat =""
+    #with open('/home/anuragr/research_persona/index.html') as fh:
+    #    dat = fh.read()
+    #return dat
+    return render_template('index.html')
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
+
+# EB looks for an 'app' callable by default.
+app = Flask(__name__)
 
 # add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: display_index()) )
+app.add_url_rule('/', 'index', (lambda: display_index()) )
+app.add_url_rule('/display/<filename>', view_func=display_image )
+
 
 # add a rule when the page is accessed with a name appended to the site
 # URL.
-#application.add_url_rule('/<username>', 'hello', (lambda username: header_text + say_hello(username) + home_link + footer_text))
+#app.add_url_rule('/<username>', 'hello', (lambda username: header_text + say_hello(username) + home_link + footer_text))
 
 # run the app.
 if __name__ == "__main__":
@@ -25,7 +47,6 @@ if __name__ == "__main__":
     if run_console:
         print(display_index())
     else:
-        application.debug = False
-        application.run()
+        app.debug = False
+        app.run()
 
-    
