5f66befd66b15c3191151e36acd1ea6caba3c9ca
Anurag S <anurag.srivastava@riskcare.com>
2021-12-08 15:26:07 +0000

    Ran hi and low subsets based on A.

--- consumption/automation/run_regression.py
+++ consumption/automation/run_regression.py
@@ -1,18 +1,20 @@
-import os,sys, subprocess
+import re,os,sys, subprocess, time
+import shutil
 
 
+def remove_if_exists(x):
+    if os.path.exists(x):
+        os.remove(x)
+                
+    
 stata_program_fpath="c:\\Program Files (x86)\\Stata14\\Stata-64.exe"
 ngr_data = {'file1':"c:/local_files/research/consumption/lsms/data/ngr_df2010.dta", 'file2':"c:/local_files/research/consumption/lsms/data/ngr_df2012.dta", 'file3':"c:/local_files/research/consumption/lsms/data/ngr_df2015.dta" }
 settings = {'NGR_direct': {'operation':'direct','data': ngr_data} , 
             'NGR_hilo_A': {'operation':'hilo','data': ngr_data , 'split_field':'lnA0'} }
 
-temp_file = 'c:/temp/resnu.tex'
+
 if not os.path.exists(stata_program_fpath):
     raise RuntimeError("Invalid Stata exec path")
-
-if os.path.exists(temp_file):
-    print("File exists")
-    os.remove(temp_file)
 
 if len(sys.argv)< 4:
     print("Must have all arguments")
@@ -31,24 +33,63 @@ if not os.path.exists(dofpath):
 if settings[settings_name]['operation'] == 'direct':
     selected_settings_data= settings[settings_name]['data']
     
+    temp_file = 'c:/temp/resnu.tex'
+    remove_if_exists(temp_file)
+    time_before_call = time.time()
+            
     retcode=subprocess.check_call([stata_program_fpath,"/e","do",dofpath,depvar,
                                    selected_settings_data['file1'],
                                    selected_settings_data['file2'],
                                    selected_settings_data['file3']])
+    
+    if (os.path.getmtime(temp_file) <time_before_call):
+        raise RuntimeError("File Pointed to is before the call")
+
     sys.exit(0)
     
 if settings[settings_name]['operation'] == 'hilo':
     source_data= settings[settings_name]['data']
-    lo_file = "c:/temp/lo.dta"
-    hi_file = "c:/temp/hi.dta"
-    try :        
-        retcode=subprocess.check_call(["Rscript", "split_data.R",source_data['file1'],
-                                       settings[settings_name]['split_field'],lo_file,hi_file])
-    except Exception as e:
-        raise e
+    lo_file_path_prefix = "c:/temp/lo_"
+    hi_file_path_prefix = "c:/temp/hi_"
+    data_files = {}
+    for file_field in ('file1','file2','file3'):
+        lo_file=lo_file_path_prefix +file_field+".dta"
+        hi_file=hi_file_path_prefix +file_field+".dta"
+        remove_if_exists(lo_file)
+        remove_if_exists(hi_file)
         
-    if retcode>0:
-        raise RuntimeError("R script failed")
+        try :
+            
+            retcode=subprocess.check_call(["Rscript", "split_data.R",source_data[file_field],
+                                           settings[settings_name]['split_field'],lo_file,hi_file])
+            
+        except Exception as e:
+            raise e
+        
+        if retcode>0:
+            raise RuntimeError("R script failed")
+        else:
+            data_files[file_field]={'lo_file':lo_file,'hi_file':hi_file}
+    print(type(data_files['file1']))       
+    print(data_files)
+    file_types = ['lo_file','hi_file']
+    for ftype in file_types :
+        temp_file = 'c:/temp/resnu.tex'
+        remove_if_exists(temp_file)
+        time_before_call = time.time()
+        print("ftype="+str(ftype))
+        
+        retcode=subprocess.check_call([stata_program_fpath,"/e","do",dofpath,depvar,
+                                   data_files['file1'][ftype],
+                                   data_files['file2'][ftype],
+                                   data_files['file3'][ftype]])
+        
+        
+        if (os.path.getmtime(temp_file) <time_before_call):
+            raise RuntimeError("File Pointed to is before the call")
+        shutil.copyfile(temp_file,re.sub("\\.tex$","_"+ftype+".tex",temp_file))
+        
+
     sys.exit(0)
 
 raise RuntimeError("Unknown operation")
