fcf5ed0d8114f2187dec3aebe839dc76247a4d62
Anurag Sriv <anandmukh@yahoo.com>
2022-02-11 11:28:38 +0000

    Minor edits.

--- consumption/datamapper/nigeria/ngr_analyse.r
+++ consumption/datamapper/nigeria/ngr_analyse.r
@@ -12,7 +12,7 @@ library(maps)
 library(ggplot2)
 library(ggrepel)
 library(acid)# for polarisation
-
+library(ineq)
 test <- function(){
   #dat <- nl@load_diary_file("../",2010,fu, ngr_normaliser, load_cost=TRUE)
   ohs <- nl@load_ohs_file(year = 2010, dirprefix = "../",fu = fu,ngrn = ngr_normaliser )
