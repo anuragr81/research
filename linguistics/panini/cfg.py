ea76cb2f4713051f25a2245dfc85b47c62cc95cb
Anurag S <anurag.srivastava@riskcare.com>
2022-04-01 22:42:43 +0100

    Adding income.

--- linguistics/panini/cfg.py
+++ linguistics/panini/cfg.py
@@ -272,8 +272,7 @@ else:
     
     print("NEXT: luNglaNglRiNgkXshvaXdudaataH has prepending issue because we don't trace insertion/prepending of vikaraNna histories. This should allow the immediate dhaatu in context.")
     # for paXtheta - we need to have for liNg : yaasuXtparasmaipadeXshuudaatto Ngichcha 3.4.103 and then ato yeyaH (because of a-ending paXtha after shap)
-    print("PENDING : murdhanyaadesha in paXthiXshyati and eruH")
-    
+    print("PENDING : murdhanyaadesha in paXthiXshyati and eruH")   
     
     
     processed_expr=(process_until_finish(expression))
