0322a8ecd4c82a36fd837a5ea1a1c5c94a3cd594
Anurag S <anandmukh@yahoo.com>
2024-07-10 22:32:23 +0100

    Adding parser for staff.

--- music/parse_music.py
+++ music/parse_music.py
@@ -1,3 +1,106 @@
+from copy import deepcopy
 
-import pyparsing
+from pyparsing import (
+    Literal,
+    Word,
+    Group,
+    Forward,
+    alphas,
+    alphanums,
+    Regex,
+    ParseException,
+    CaselessKeyword,
+    Suppress,
+    delimitedList,
+)
 
+
+def push_first(toks):
+    return None
+
+def push_unary_minus(toks):
+    return None
+
+def get_calculator_expression():
+#   https://github.com/pyparsing/pyparsing/blob/master/examples/SimpleCalc.py
+    e = CaselessKeyword("E")
+    pi = CaselessKeyword("PI")
+    
+    fnumber = Regex(r"[+-]?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?")
+    ident = Word(alphas, alphanums + "_$")
+    
+    plus, minus, mult, div = map(Literal, "+-*/")
+    lpar, rpar = map(Suppress, "()")
+    addop = plus | minus
+    multop = mult | div
+    expop = Literal("^")
+    
+    expr = Forward()
+    expr_list = delimitedList(Group(expr))
+    # add parse action that replaces the function identifier with a (name, number of args) tuple
+     
+    def insert_fn_argcount_tuple(t):
+        fn = t.pop(0)
+        num_args = len(t[0])
+        t.insert(0, (fn, num_args))
+    
+    fn_call = (ident + lpar - Group(expr_list) + rpar).setParseAction( insert_fn_argcount_tuple)
+    # addop[...] just means ZeroOrMore(addop)
+    atom = (
+      addop[...]
+      + (
+      (fn_call | pi | e | fnumber | ident).setParseAction(push_first)
+      | Group(lpar + expr + rpar)
+      )
+      ).setParseAction(push_unary_minus)
+    
+    factor = Forward()
+    factor <<= atom + (expop + factor).setParseAction(push_first)[...]
+    term = factor + (multop + factor).setParseAction(push_first)[...]
+    expr <<= term + (addop + term).setParseAction(push_first)[...]
+    return expr
+
+
+def add_staff(toks):
+    print (toks)
+    
+
+def add_unit(toks):
+    global taalaqueue
+    global notesqueue 
+    taalaqueue.append(deepcopy(notesqueue ))
+    notesqueue = []
+    
+    
+def add_note(toks):
+    if len(toks)>1:
+        raise RuntimeError("cannot have more than one note within the same token")
+    global notesqueue 
+
+    notesqueue.append(toks[0])
+    
+def grammar():
+    gap = Literal("-")
+    comma = Literal(",")
+    lpar, rpar = map(Literal, "()")
+    lsqb, rsqb= map(Literal, "[]")
+    singlenote = ( gap| Word(alphanums) ) .setParseAction(add_note)
+    singleunit = (lpar + delimitedList(Group(singlenote)) [...] + rpar | gap).setParseAction(add_unit)
+    staff = (lsqb + delimitedList(Group(singleunit)) + rsqb)
+    return staff
+    
+if __name__ == '__main__':
+    #input_string = "((A1,A2,C3),(A2,-,A3))"
+    input_string = "[ (A1,A2),(A2,-)]"
+    notesqueue = []
+    taalaqueue =[]
+    pattern = grammar()
+    if input_string != "":
+            # try parsing the input string
+            try:
+                L = pattern.parseString(input_string, parseAll=True)
+            except ParseException as err:
+                L = ["Parse Failure", input_string, (str(err), err.line, err.column)]
+                print(L)
+
+    print("TEST")
